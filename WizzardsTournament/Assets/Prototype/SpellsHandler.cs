69dd8cae85401770723f567c6bc69ca92883d339
Fusion <fusion@fusiongamestudios.com>
2016-08-29 14:23:33 -0400

    Added the ability to throw balls

---
+++ WizzardsTournament/Assets/Prototype/SpellsHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Prototype
+{
+    public class SpellsHandler : MonoBehaviour
+    {
+
+        public Transform spellsSpawningPosition;
+        public Rigidbody spell;
+        public float force;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Rigidbody currentSpell = Instantiate(spell,spellsSpawningPosition.position,spellsSpawningPosition.rotation) as Rigidbody;
+                currentSpell.AddForce(currentSpell.transform.forward * force);
+            }
+        }
+    }
+
+}
