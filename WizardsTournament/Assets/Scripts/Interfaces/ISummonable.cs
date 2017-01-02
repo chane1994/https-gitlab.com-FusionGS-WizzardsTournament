deaf8f77281e2a9e86d1c7f3ff144e41670b12cd
Fusion <fusion@fusiongamestudios.com>
2017-01-02 10:09:46 -0500

    the spell seal recognizes a good and a bad combination

---
+++ WizardsTournament/Assets/Scripts/Interfaces/ISummonable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WizardsTournament
+{
+    /// <summary>
+    /// Represents all spells, creatures, or avilities that can be summoned using the spell seal 
+    /// </summary>
+    public interface ISummonable
+    {
+         void Activate();
+    }
+
+}
--- WizardsTournament/Assets/Scripts/Spells/SpellSeal.cs
+++ WizardsTournament/Assets/Scripts/Spells/SpellSeal.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 
 namespace WizardsTournament
 {
@@ -8,20 +10,27 @@ namespace WizardsTournament
     /// </summary>
     public class SpellSeal : MonoBehaviour
     {
+        #region Variables
         public Collider[] validColliders;
-        SymbolHandler[] _symbolHandlers;
-        int _firstSymbolindex = -1;
-
+        SymbolHandler _firstSymbolHandler;
         protected float lifeTime = 8;
+        StringBuilder _currentCombination;
+        protected KeyValuePair<string, ISummonable>[] _validCombinations;
+        bool _activateFirstSymbol = false;
+        #endregion
 
+        #region Methods
         void Start()
         {
+            //TODO populate the validCombinations array
+            _validCombinations = new KeyValuePair<string, ISummonable>[2] { new KeyValuePair<string, ISummonable>("LightFireLight",null), new KeyValuePair<string, ISummonable>("StrengthFireLightStrength", null) };
+
             Invoke("DestroySpellSeal", lifeTime);
             //   
-            _symbolHandlers = GetComponentsInChildren<SymbolHandler>();
-            for (int i = 0; i < _symbolHandlers.Length; i++)
+            SymbolHandler[] symbolHandlers = GetComponentsInChildren<SymbolHandler>();
+            for (int i = 0; i < symbolHandlers.Length; i++)
             {
-                _symbolHandlers[i].symbolSummonerEventHandler = new SymbolHandler.SymbolSummoner(OnSymbolSelected);
+                symbolHandlers[i].symbolSummonerEventHandler = new SymbolHandler.SymbolSummoner(OnSymbolSelected);
             }
         }
 
@@ -36,31 +45,53 @@ namespace WizardsTournament
         /// </summary>
         /// <param name="collider"></param>
         /// <param name="symbol">Symbol of the trigger that was touched</param>
-        void OnSymbolSelected(Collider collider, Symbol symbol)
+        void OnSymbolSelected(SymbolHandler symbolHandler, Collider collider) 
         {
             if(validColliders[0].Equals(collider) || validColliders[1].Equals(collider)) //avoids that the seal activates with something different than the summoner's hands
             {
-                for (int i = 0; i < _symbolHandlers.Length; i++) //find the selected symbol and deactivate the trigger
+                if (_firstSymbolHandler == null)
                 {
-                    if (_symbolHandlers[i].symbol.Equals(symbol))
+                    _firstSymbolHandler = symbolHandler;
+                    _currentCombination = new StringBuilder(symbolHandler.symbol.ToString());
+                    _activateFirstSymbol = true;
+                }
+                else
+                {
+                    _currentCombination.Append(symbolHandler.symbol.ToString());
+                    if (_activateFirstSymbol)
                     {
-
-                        if (_firstSymbolindex < 0) //if it was the first one save the index because it will be activated later
-                        {
-                            _firstSymbolindex = i;
-                        }
-                        else
-                        {
-                            _symbolHandlers[_firstSymbolindex].GetComponent<MeshCollider>().enabled = true;
-                        }
-
-                        _symbolHandlers[i].GetComponent<MeshCollider>().enabled = false;
-                        break;
+                        _firstSymbolHandler.GetComponent<MeshCollider>().enabled = true;
+                        _activateFirstSymbol = false;
+                    }
+                    //check to see if it is the first symbol handler
+                    if (symbolHandler.Equals(_firstSymbolHandler))
+                    {
+                        TrySummonSpell();
                     }
                 }
-                Debugger.Log(symbol.ToString());
+                Debugger.Log(symbolHandler.symbol.ToString());
             }
         }
+
+        /// <summary>
+        /// Compares the current combination to all the valid ones and executes the ISummonable if it finds a match.
+        /// </summary>
+        void TrySummonSpell()
+        {
+            string currentCombination = _currentCombination.ToString();
+            for (int i = 0; i < _validCombinations.Length; i++)
+            {
+                if (currentCombination.Equals(_validCombinations[i].Key))
+                {
+                    Debugger.Log("It is a valid combination");
+                   // _validCombinations[i].Value.Activate();
+                    break;
+                }
+            }
+
+            DestroySpellSeal();
+        }
+        #endregion
     }
 
 }
